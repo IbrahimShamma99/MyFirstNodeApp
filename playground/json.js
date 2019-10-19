803986978bfb4053aff3e9a32af1b9b7c5957a61
Ibrahim shamma <i.abushammah@gmail.com>
2019-10-19 13:48:10 +0300

    jsons

---
+++ playground/json.js
@@ -0,0 +1,25 @@
+const fs = require("fs")
+
+// data = {
+//     name:"andrew",
+//     planet:"earth",
+//     age:27
+// }
+
+// const datajson = JSON.stringify(data)
+
+//fs.writeFileSync( "1-json.json" ,datajson)
+
+const filename = "1-json.json"
+
+const databufferjson = fs.readFileSync(filename)
+const databuffer  = databufferjson.toString()
+const data = JSON.parse(databuffer)
+
+data.name = "ibrahim"
+data.planet = "heaven"
+data.age = 20
+
+const datajson = JSON.stringify(data)
+
+fs.writeFileSync(filename,datajson)
